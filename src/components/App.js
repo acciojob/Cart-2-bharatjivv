41a88b9284234a41fd54f29f16b8451fa474c63a
BharatJivvTate <hibharatj@gmail.com>
2025-09-17 12:06:48 +0530

    [fix] - App.js conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,8 @@
-<<<<<<< HEAD
 import React from 'react';
 import Navbar from "./Navbar";
 import CartList from "./CartList";
 import { CartProvider } from '../context/CartContext';
-=======
->>>>>>> 9ffa55ce7d413ec5d5f87b6e20c906d27b963544
+
 
 const App = () => {
   return (
